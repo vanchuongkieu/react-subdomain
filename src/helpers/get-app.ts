2c45b7d7c9233663714b27c4435dabb52519e96c
Chuong <chuong@Chuongs-Mac-mini.local>
2022-10-29 14:00:30 +0700

    5

--- src/helpers/get-app.ts
+++ src/helpers/get-app.ts
@@ -7,13 +7,15 @@ export const initializeApp = () => {
 
     const mainApp = APPS.find(app => app.main)!;
     if (!subdomain) return mainApp.router;
-
+    console.log(subdomain);
+    
     const appSubdomain = APPS.find(app => app.subdomain === subdomain)!;
     if (appSubdomain) return appSubdomain.router;
     return NotFound;
 }
 
 const setupSubdomain = (location: string) => {
+    location = location.replace('www.', '')
     const parts = location.split('.');
     const isLocalhost = parts.slice(-1)[0] === 'localhost';
     const sliceStill = isLocalhost ? -1 : -2;
